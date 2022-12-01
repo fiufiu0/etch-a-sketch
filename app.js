606c887a2297539e5081a022a0778ee71f8166cc
Łukasz <nipseyyo@gmail.com>
2022-12-01 20:01:57 +0100

    Const DOM  methods

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const main = document.querySelector('.main')
+const main = document.querySelector('.main');
+const colorBtn = document.querySelector('.btn-color');
+const picker = document.getElementById('picker');
+const rainbow = document.getElementById('rainbow');
+const clear = document.getElementById('clear');
+const range = document.getElementById('range');
 
 
 function getGrid(size){
     for(let i = 1; i <= size * size; i++){
             const div = document.createElement('div');
             div.classList.add('grid')
+
+            div.addEventListener('mouseover', changeColor);
+            div.addEventListener('mousedown', changeColor);
+
             main.appendChild(div)
     }
 }
 
 getGrid(16);
 
+
+function changeColor(e){
+    e.target.style.background = 'red'
+
+    console.log(e.type)
+}
